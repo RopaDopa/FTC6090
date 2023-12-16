2a036d9240682a3cc2bd6b96f76e7e45a16b240a
Talitha Roper <r70talshuri@yahoo.com>
2023-12-15 21:08:43 -0500

    Added encoder drive for hangMotor andnsimplified controls

--- FinalBotCode/src/main/java/org/firstinspires/ftc/FinalBotCode/MHardware.java
+++ FinalBotCode/src/main/java/org/firstinspires/ftc/FinalBotCode/MHardware.java
@@ -21,20 +21,23 @@ public class MHardware {
     private Servo angleDroneServo = null; //Servo
     private Servo launchDroneServo = null; //Servo
 
+    private ElapsedTime     runtime = new ElapsedTime();
+
     //Constants
     public static final double GRAB_PIXEL = 0;
     public static final double RELEASE_PIXEL = 45;
     public static final double ANGLE_DRONE = 45;
-    public static final double ANGLE_DRONE_RESTPOS = 90;
+    public static final double ANGLE_DRONE_RESTPOS = 45;
     public static final double LAUNCH_DRONE = 90;
     public static final double INTAKELIFT_BACKPOS = 0;
     public static final double INTAKELIFT_STRAIGHTPOS = 45;
     public static final double INTAKELIFT_FORWARDPOS = 90;
     public static final double INTAKE_COLLECTPOS = 90;
+    public static final double INTAKE_BBPOS = 45;
     public static final double INTAKE_RESTPOS = 0;
     public static final double LIFT_INTAKE = 0.8;
     public static final double HANG_ROBOT = 0.5;
-    public static final double LIFT_COUNTSPERINCH = 0;
+    public static final double HANGLIFT_COUNTSPERINCH = 2357;
 
     public MHardware(LinearOpMode opmode) {
         finalOpMode = opmode;
@@ -58,8 +61,6 @@ public class MHardware {
         hangMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         liftIntakeMotor = finalOpMode.hardwareMap.get(DcMotor.class, "liftIntakeMotor");
-        liftIntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftIntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         intakeServo = finalOpMode.hardwareMap.get(Servo.class, "intakeServo");
         angleIntakeLiftServo = finalOpMode.hardwareMap.get(Servo.class, "angleIntakeLiftServo");
@@ -70,8 +71,6 @@ public class MHardware {
         //Letting the users know that the robot is fully initialized
         finalOpMode.telemetry.addData("Status", "Robot Ready");
         finalOpMode.telemetry.update();
-
-
     }
 
     //To set the powers for the Mecanum wheels
@@ -105,20 +104,38 @@ public class MHardware {
     }
 
     //HANGING MECHANISM TO LIFT ROBOT (motor)
-    public void hangRobot(double hang) {
-        setHangPower(hang);
-    }
+    public void hangRobot(double speed, double hangLiftInches, double timeoutS) {
+        int newHangMotorTarget;
+        double hangLiftInchesTotal;
 
-    public void setHangPower(double hangInput) {
-        hangMotor.setPower(hangInput);
+        newHangMotorTarget = hangMotor.getCurrentPosition() + (int)(hangLiftInches * HANGLIFT_COUNTSPERINCH);
+
+        hangMotor.setTargetPosition(newHangMotorTarget);
+        hangMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        runtime.reset();
+        hangMotor.setPower(Math.abs(speed));
+
+        while (finalOpMode.opModeIsActive() && (runtime.seconds() < timeoutS) && (hangMotor.isBusy())) {
+
+            // Display it for the driver.
+            finalOpMode.telemetry.addData("Running to", "%7d", newHangMotorTarget);
+            finalOpMode.telemetry.addData("Currently at", "at %7d", hangMotor.getCurrentPosition());
+            finalOpMode.telemetry.update();
+
+        }
+        // Stop all motion;
+        hangMotor.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        hangMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        finalOpMode.sleep(250);
     }
 
     //LIFT THE INTAKE  (motor)
-//    public void liftIntakeEncoder(double speed, double liftMotorInches, double timeoutS) {
-//        int newLiftMotorTarget;
-//        newLiftMotorTarget = liftIntakeMotor.getCurrentPosition() + (int)(liftMotorInches * LIFT_COUNTSPERINCH);
-//        liftIntakeMotor.setPower(lift);
-//    } //^UNDER CONSTRUCTION TO MOVE LIFT USING ENCODER COUNTS^
+    public void liftIntake(double lift) {
+        liftIntakeMotor.setPower(lift);
+    } //^UNDER CONSTRUCTION TO MOVE LIFT USING ENCODER COUNTS^
 
     //OPEN/CLOSE THE INTAKE (servo)
     public void grabPixel(double grab) {
@@ -140,92 +157,12 @@ public class MHardware {
     }
 
     //ANGLE THE LIFT FOR THE INTAKE (linear actuator)
-    public void posAngleBack(double angleBack) {
-        angleIntakeLiftServo.setPosition(angleBack);
-    }
-
-    public void posAngleStraight(double angleStraight) {
-        angleIntakeLiftServo.setPosition(angleStraight);
-    }
-
-    public void posAngleForward(double angleForward) {
-        angleIntakeLiftServo.setPosition(angleForward);
+    public void intakeLiftAngle(double liftPosition) {
+        angleIntakeLiftServo.setPosition(liftPosition);
     }
 
     //ANGLE THE INTAKE (linear actuator)
-    public void intakeCollectPos(double collect) {
-        angleIntakeServo.setPosition(collect);
+    public void intakeAngle(double position) {
+        angleIntakeServo.setPosition(position);
     }
-
-    public void intakeRestPos(double rest) {
-        angleIntakeServo.setPosition(rest);
-    }
-
-    public void encoderDrive(double speed,
-                             double leftFrontInches, double leftRearInches, double rightFrontInches, double rightRearInches,
-                             double timeoutS) {
-        int newLeftFrontTarget;
-        int newLeftRearTarget;
-        int newRightFrontTarget;
-        int newRightRearTarget;
-
-        // Ensure that the OpMode is still active
-//        if (opModeIsActive()) {
-//
-//            // Determine new target position, and pass to motor controller
-//            newLeftFrontTarget = leftFront.getCurrentPosition() + (int)(leftFrontInches * COUNTS_PER_INCH);
-//            newLeftRearTarget = leftRear.getCurrentPosition() + (int)(leftRearInches * COUNTS_PER_INCH);
-//            newRightFrontTarget = rightFront.getCurrentPosition() + (int)(rightFrontInches * COUNTS_PER_INCH);
-//            newRightRearTarget = rightRear.getCurrentPosition() + (int)(rightRearInches * COUNTS_PER_INCH);
-//            leftFront.setTargetPosition(newLeftFrontTarget);
-//            leftRear.setTargetPosition(newLeftRearTarget);
-//            rightFront.setTargetPosition(newRightFrontTarget);
-//            rightRear.setTargetPosition(newRightRearTarget);
-//
-//            // Turn On RUN_TO_POSITION
-//            leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//            rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//            // reset the timeout time and start motion.
-//            runtime.reset();
-//            leftFront.setPower(Math.abs(speed));
-//            leftRear.setPower(Math.abs(speed));
-//            rightFront.setPower(Math.abs(speed));
-//            rightRear.setPower(Math.abs(speed));
-//
-//            // keep looping while we are still active, and there is time left, and both motors are running.
-//            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-//            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-//            // always end the motion as soon as possible.
-//            // However, if you require that BOTH motors have finished their moves before the robot continues
-//            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-//            while (opModeIsActive() &&
-//                    (runtime.seconds() < timeoutS) &&
-//                    (leftFront.isBusy() && rightFront.isBusy() && leftRear.isBusy() && rightRear.isBusy())) {
-//
-//                // Display it for the driver.
-//                telemetry.addData("Running to",  " %7d :%7d :%7d :%7d", newLeftFrontTarget, newLeftRearTarget, newRightFrontTarget, newRightRearTarget);
-//                telemetry.addData("Currently at",  " at %7d :%7d :%7d :%7d",
-//                        leftFront.getCurrentPosition(), leftRear.getCurrentPosition(), rightFront.getCurrentPosition(), rightRear.getCurrentPosition());
-//                telemetry.update();
-//            }
-//
-//            // Stop all motion;
-//            leftFront.setPower(0);
-//            leftRear.setPower(0);
-//            rightFront.setPower(0);
-//            rightRear.setPower(0);
-//
-//            // Turn off RUN_TO_POSITION
-//            leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//            rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//
-//            sleep(250);   // optional pause after each move.
-//        }
-//    } //^Under construction to borrow code to move the lifts with encoder counts^
-    }
-}h
+}
