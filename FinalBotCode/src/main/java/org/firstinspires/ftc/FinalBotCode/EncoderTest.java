ec6f7a2841d1bb24c0fff64895e59f50265eaf01
Talitha Roper <r70talshuri@yahoo.com>
2023-12-15 21:07:13 -0500

    To test for counts and get counts per inch for hang and angle intake motors

--- FinalBotCode/src/main/java/org/firstinspires/ftc/FinalBotCode/EncoderTest.java
+++ FinalBotCode/src/main/java/org/firstinspires/ftc/FinalBotCode/EncoderTest.java
@@ -3,22 +3,26 @@ package org.firstinspires.ftc.FinalBotCode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 @TeleOp
 public class EncoderTest extends LinearOpMode {
     //Variables to store the hardware position for the motors
     private DcMotor liftIntakeMotor = null;
     private DcMotor hangMotor = null;
     public void runOpMode() {
-        //Getting the motors fromt the hardware configuration
+        //Getting the motors from the hardware configuration
         liftIntakeMotor = hardwareMap.get(DcMotor.class, "liftIntakeMotor");
         hangMotor = hardwareMap.get(DcMotor.class, "hangMotor");
+
+        liftIntakeMotor.setDirection(DcMotor.Direction.REVERSE);
+        hangMotor.setDirection(DcMotor.Direction.REVERSE);
 
         //Resetting the previous encoder counts
         liftIntakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         hangMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        //Setting the motors to run with encoders
-        liftIntakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //Setting the motor to run with encoders
         hangMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         //Variables to store the position before starting
@@ -34,26 +38,23 @@ public class EncoderTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             //Storing the controls to move the motors in variables.
-            double drive = gamepad1.left_stick_y;
-            double hang = gamepad1.right_stick_y;
+            double drive = -gamepad1.left_stick_y;
+            double hang = -gamepad1.right_stick_y;
 
-            //Assigning the position of the motors (the current encoder counts) to variables
-            double position = liftIntakeMotor.getCurrentPosition();
-            double hangPos = hangMotor.getCurrentPosition();
-
-            //Storing value of mode of motors in string to use in telemetry (don't know if it works)
+            //Storing value of mode of motors in string to use in telemetry
             String hangMotorMode = String.valueOf(hangMotor.getMode());
             String liftIntakeMotorMode = String.valueOf(liftIntakeMotor.getMode());
 
             //Allowing to motor to move based on the gamepad input
-            liftIntakeMotor.setPower(drive);
-            hangMotor.setPower(hang);
+            liftIntakeMotor.setPower(Math.abs(drive));
+            hangMotor.setPower(Math.abs(hang));
 
             //Telemetry to see the encoder counts
             telemetry.addData("Lift Running Mode", liftIntakeMotorMode);
             telemetry.addData("Hang Motor Running Mode", hangMotorMode);
-            telemetry.addData("Lift Encoder Counts", position);
-            telemetry.addData("Hang Motor Encoder Counts", hangPos);
+            telemetry.addData("~~~~~~~~~", "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            telemetry.addData("Lift Encoder Counts", "%7d", liftIntakeMotor.getCurrentPosition());
+            telemetry.addData("Hang Motor Encoder Counts", "%7d", hangMotor.getCurrentPosition());
             telemetry.update();
 
             sleep(50);
